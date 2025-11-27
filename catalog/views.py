34d49648e343e551533e94d2e4d2a143cd270946
gr434_bedti <sunnata30@gmail.com>
2025-11-27 09:34:39 +0700

    Раздел 8 пройден

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
 
@@ -50,3 +51,17 @@ class AuthorDetailView(generic.DetailView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
