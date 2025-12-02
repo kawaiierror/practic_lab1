bd690c7af48c0d565e88ab0e0d06bfec3b1dbd87
gr434_bedti <sunnata30@gmail.com>
2025-12-02 13:03:14 +0700

    Финальная версия

--- catalog/views.py
+++ catalog/views.py
@@ -22,7 +22,6 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
 
-    # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     num_visits += 1
     request.session['num_visits'] = num_visits
